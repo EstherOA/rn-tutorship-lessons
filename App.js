be6081a39c8148de50dabdb6f8c879cb2e094835
EstherOA <estherowusuansah19@gmail.com>
2021-06-30 20:38:28 +0000

    conditional rendering cont.

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import MoodEntryComponent from './src/components/mood-entry/MoodEntryComponent';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, StyleSheet, FlatList, ActivityIndicator} from 'react-native';
+
+import CardComponent from './src/components/card/CardComponent';
 
 export default function App() {
+  const moods = [
+    {title: 'Hello', id: 1},
+    {title: 'Hey', id: "hi"},
+    {title: 'I love React', id: 3},
+  ];
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
+  const renderItem = ({item}) => <CardComponent title={item.title} />;
 
   return (
     <SafeAreaView style={styles.container}>
-        <MoodEntryComponent initialMood="I'm happy"/>
-        <MoodEntryComponent initialMood="I'm so sad!"/>
+      {loading ? (
+        <ActivityIndicator size="large" color="black" />
+      ) : (
+        <FlatList
+          renderItem={renderItem}
+          data={moods}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
     </SafeAreaView>
   );
 }
@@ -15,6 +35,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    paddingTop: 50
   },
 });
