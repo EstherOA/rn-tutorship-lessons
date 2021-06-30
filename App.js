c91d3644e590c531e5cc4e2898f706642f820985
EstherOA <estherowusuansah19@gmail.com>
2021-06-30 19:58:40 +0000

    using props

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import ButtonComponent from './src/components/ButtonComponent';
 import CardComponent from './src/components/CardComponent';
-import CatClassComponent from './src/components/CatClassComponent';
-import CatFunctionalComponent from './src/components/CatFunctionalComponent';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-     <CatClassComponent/>
-     <CatFunctionalComponent/>
-     <ButtonComponent/>
-     <CardComponent/>
+     <CardComponent title="Hello There"/>
+     <CardComponent title="Hello World"/>
+     <CardComponent title="Hello Micah!"/>
     </SafeAreaView>
   );
 }
--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,87 +1,36 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-const CardComponent = () => {
+const CardComponent = props => {
+  const {title} = props;
+
   return (
-    <View>
-      <View style={styles.cardView}>
-        <View style={styles.headerView}>
-          <Text style={styles.title}>I am a Card</Text>
-        </View>
-        <View style={styles.cardContentView}>
-          <View style={styles.leftContent}>
-            <Image
-              style={styles.cardImage}
-              source={require('../../assets/man.jpg')}
-            />
-          </View>
-          <View style={styles.rightContent}>
-            <Text style={styles.cardContentText}>Esther Mingle</Text>
-            <Text style={styles.cardContentText}>Age: 13</Text>
-            <Text style={styles.cardContentText}>Doctor</Text>
-          </View>
-        </View>
-      </View>
+    <View
+      style={[styles.cardContainer, {backgroundColor: getRandomColor(colors)}]}>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
-export default CardComponent;
+const getRandomColor = arr => {
+  var item = arr[Math.floor(Math.random() * arr.length)];
+  return item;
+};
+
+const colors = ['#F2994A', '#2F80ED', '#56CCF2', '#BB6BD9', '#EB5757'];
 
 const styles = StyleSheet.create({
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
-  cardView: {
-    marginHorizontal: 20,
-    elevation: 3,
-    backgroundColor: '#f2f2f2',
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
-  headerView: {
-    backgroundColor: 'darkgreen',
-    padding: 10,
+  cardContainer: {
     width: '100%',
-    height: 50,
+    height: 100,
+    marginBottom: 20,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  title: {
-    fontWeight: '700',
-    textTransform: 'uppercase',
-    color: 'white',
-    textAlign: 'center',
-    width: '100%',
+  text: {
+    color: '#fff',
+    fontSize: 34,
   },
-  cardContentView: {
-    width: '100%',
-    height: 200,
-    color: 'black',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  leftContent: {
-    width: '40%',
-    height: '100%',
-  },
-  rightContent: {
-    width: '60%',
-    height: '100%',
-    paddingLeft: 20,
-  },
-  cardImage: {
-    width: 150,
-    resizeMode: 'cover',
-    height: 200,
-  },
-  introText: {
-    color: 'black',
-  },
-  cardContentText: {
-      fontSize: 18
-  }
 });
 
+export default CardComponent;
